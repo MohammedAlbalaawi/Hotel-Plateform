6a822d1a563a59057af70f341a56a1a84057067d
MohammedAlbalaawi <m.cip@live.com>
2022-11-13 15:40:25 +0200

    Connect Slider in Front home page with backend -make it dynamic-

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Slider;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('front.home');
+        $all_slides = Slider::get();
+        return view('front.home',compact('all_slides'));
     }
 }
