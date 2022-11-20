79b428a0cd22495d6d0d38f76089bf381d9c11c3
MohammedAlbalaawi <m.cip@live.com>
2022-11-20 15:48:52 +0200

    Show Testimonials in front page

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Feature;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 use App\Models\Slider;
 
@@ -12,6 +13,7 @@ class HomeController extends Controller
     public function index(){
         $all_slides = Slider::get();
         $features = Feature::get();
-        return view('front.home',compact('all_slides','features'));
+        $testimonials = Testimonial::get();
+        return view('front.home',compact('all_slides','features', 'testimonials'));
     }
 }
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -245,34 +245,25 @@
             <div class="row">
                 <div class="col-12">
                     <div class="testimonial-carousel owl-carousel">
+                        @forelse($testimonials as $testimonial)
                         <div class="item">
                             <div class="photo">
-                                <img src="uploads/t1.jpg" alt="">
+                                <img src="{{\Illuminate\Support\Facades\Storage::url( $testimonial->photo )}}" alt="">
                             </div>
                             <div class="text">
-                                <h4>Robert Krol</h4>
-                                <p>CEO, ABC Company</p>
+                                <h4>{{$testimonial->name}}</h4>
+                                <p>{{$testimonial->career}}</p>
                             </div>
                             <div class="description">
                                 <p>
-                                    Lorem ipsum dolor sit amet, an labores explicari qui, eu nostrum copiosae argumentum has. Latine propriae quo no, unum ridens. Lorem ipsum dolor sit amet, an labores explicari qui, eu nostrum copiosae argumentum has. Latine propriae quo no, unum ridens.
+                                    {{$testimonial->comment}}
                                 </p>
                             </div>
                         </div>
-                        <div class="item">
-                            <div class="photo">
-                                <img src="uploads/t2.jpg" alt="">
-                            </div>
-                            <div class="text">
-                                <h4>Sal Harvey</h4>
-                                <p>Director, DEF Company</p>
-                            </div>
-                            <div class="description">
-                                <p>
-                                    Lorem ipsum dolor sit amet, an labores explicari qui, eu nostrum copiosae argumentum has. Latine propriae quo no, unum ridens. Lorem ipsum dolor sit amet, an labores explicari qui, eu nostrum copiosae argumentum has. Latine propriae quo no, unum ridens.
-                                </p>
-                            </div>
-                        </div>
+                        @empty
+                            <p>No Data</p>
+                        @endforelse
+
                     </div>
                 </div>
             </div>
