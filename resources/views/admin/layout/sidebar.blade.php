94091ea66f2a4fe76a54f43d432a6c4f18c20633
MohammedAlbalaawi <m.cip@live.com>
2022-12-05 11:34:17 +0200

    Fix: make selected tab active in Sidebar menu

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -9,24 +9,24 @@
 
         <ul class="sidebar-menu">
 
-            <li class="active"><a class="nav-link" href="{{route('admin_home')}}"><i class="fa fa-hand-o-right"></i> <span>Dashboard</span></a></li>
+            <li class="{{Request::is('admin/home') ? 'active' : ''}}"><a class="nav-link" href="{{route('admin_home')}}"><i class="fa fa-hand-o-right"></i> <span>Dashboard</span></a></li>
 
 
-            <li class=""><a class="nav-link" href="{{ route('pages.index') }}"><i class="fa fa-hand-o-right"></i> <span>Static Pages</span></a></li>
+            <li class="{{Request::is('pages') || Request::is('pages/*') ? 'active' : ''}}"><a class="nav-link" href="{{ route('pages.index') }}"><i class="fa fa-hand-o-right"></i> <span>Static Pages</span></a></li>
 
-            <li class=""><a class="nav-link" href="{{ route('adminSlider.index') }}"><i class="fa fa-hand-o-right"></i> <span>ٍSlides</span></a></li>
+            <li class="{{Request::is('admin/slide/*') ? 'active' : ''}}"><a class="nav-link" href="{{ route('adminSlider.index') }}"><i class="fa fa-hand-o-right"></i> <span>ٍSlides</span></a></li>
 
-            <li class=""><a class="nav-link" href="{{ route('adminFeature.view') }}"><i class="fa fa-hand-o-right"></i> <span>Features</span></a></li>
+            <li class="{{Request::is('admin/feature/*') ? 'active' : ''}}"><a class="nav-link" href="{{ route('adminFeature.view') }}"><i class="fa fa-hand-o-right"></i> <span>Features</span></a></li>
 
-            <li class=""><a class="nav-link" href="{{ route('testimonials.index') }}"><i class="fa fa-hand-o-right"></i> <span>Testimonials</span></a></li>
+            <li class="{{Request::is('testimonials') || Request::is('testimonials/*') ? 'active' : ''}}"><a class="nav-link" href="{{ route('testimonials.index') }}"><i class="fa fa-hand-o-right"></i> <span>Testimonials</span></a></li>
 
-            <li class=""><a class="nav-link" href="{{ route('posts.index') }}"><i class="fa fa-hand-o-right"></i> <span>Posts</span></a></li>
+            <li class="{{Request::is('posts') || Request::is('posts/*') ? 'active' : ''}}"><a class="nav-link" href="{{ route('posts.index') }}"><i class="fa fa-hand-o-right"></i> <span>Posts</span></a></li>
 
-            <li class=""><a class="nav-link" href="{{ route('photos.index') }}"><i class="fa fa-hand-o-right"></i> <span>Photo Gallery</span></a></li>
+            <li class="{{Request::is('photos') || Request::is('photos/*') ? 'active' : ''}}"><a class="nav-link" href="{{ route('photos.index') }}"><i class="fa fa-hand-o-right"></i> <span>Photo Gallery</span></a></li>
 
-            <li class=""><a class="nav-link" href="{{ route('videos.index') }}"><i class="fa fa-hand-o-right"></i> <span>Video Gallery</span></a></li>
+            <li class="{{Request::is('videos') || Request::is('videos/*') ? 'active' : ''}}"><a class="nav-link" href="{{ route('videos.index') }}"><i class="fa fa-hand-o-right"></i> <span>Video Gallery</span></a></li>
 
-            <li class=""><a class="nav-link" href="{{ route('faqs.index') }}"><i class="fa fa-hand-o-right"></i> <span>Faqs</span></a></li>
+            <li class="{{Request::is('faqs') || Request::is('faqs/*') ? 'active' : ''}}"><a class="nav-link" href="{{ route('faqs.index') }}"><i class="fa fa-hand-o-right"></i> <span>Faqs</span></a></li>
 
         </ul>
     </aside>
