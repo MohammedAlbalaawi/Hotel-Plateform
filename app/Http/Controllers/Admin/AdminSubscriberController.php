a644a22a5e3d1356ffae7ca3f2b2277762f41a6d
MohammedAlbalaawi <m.cip@live.com>
2022-12-08 16:36:40 +0200

    Send Email to all verified usere

--- app/Http/Controllers/Admin/AdminSubscriberController.php
+++ app/Http/Controllers/Admin/AdminSubscriberController.php
@@ -3,13 +3,33 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SubscriberSendEmailRequest;
+use App\Mail\Websitemail;
 use App\Models\Subscribe;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class AdminSubscriberController extends Controller
 {
     public function index(){
         $subscribers = Subscribe::where('status',1)->get();
         return view('admin.subscribers.index', compact('subscribers'));
+    }
+
+    public function create(){
+        return view('admin.subscribers.create');
+    }
+
+    public function store(SubscriberSendEmailRequest $request){
+        $subject = $request->subject;
+        $message = $request->message;
+
+        $verifiedSubscribers = Subscribe::where('status',1)->get();
+
+        foreach ($verifiedSubscribers as $subscriber){
+            Mail::to($subscriber->email)->send(new Websitemail($subject, $message));
+        }
+
+        return redirect()->back()->with('success','Email has been sent successfully');
     }
 }
